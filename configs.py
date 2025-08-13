ca2779ae2f9bebd1db36a40f751718002a5ca42c
aurigacosmic43 <asu118836@gmail.com>
2025-08-13 18:17:10 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,19 +8,19 @@ import os
 
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	API_ID = int(os.environ.get("API_ID", 12345))
-	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN" , "7628459480:AAH-5SlJj5Yvw44ro9ms_Yf0oYO-WbWUKwE")
+	API_ID = int(os.environ.get("API_ID", "20202379"))
+	API_HASH = os.environ.get("API_HASH" , "cb1d30a2facf3a1d5691fe3dbe8e8482")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL" , "-1002872705275"))
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-	CAPTION = "By @AHToolsBot"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	OWNER_ID = int(os.environ.get("OWNER_ID", "8439944234"))
+	CAPTION = "By @Spicyxhub"
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "newwmmbot")
+	DATABASE_URL = os.environ.get("DATABASE_URL" , "mongodb+srv://cosmiccrazy16:8D2FjwQS3ky9y5Nq@cluster0.cue31.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", False))
 	USAGE_WATERMARK_ADDER = """
